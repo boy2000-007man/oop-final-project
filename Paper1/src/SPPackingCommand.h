9f88f78de3f52ee011f753a06020371b2e8f4b92
zero <boy2000007man@gmail.com>
2013-06-28 14:51:09 +0800

    add new functions, setS1() and setS2()

--- Paper1/src/SPPackingCommand.h
+++ Paper1/src/SPPackingCommand.h
@@ -17,6 +17,8 @@ public:
   void next();
   void getS1(std::vector<int> &s1) { s1 = m_s1; }
   void getS2(std::vector<int> &s2) { s2 = m_s2; }
+  void setS1(std::vector<int> &s1) { m_s1 = s1; }
+  void setS2(std::vector<int> &s2) { m_s2 = s2; }
 
 protected:
   void change(std::vector<int> &m_s);
