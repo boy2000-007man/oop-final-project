4ac7027c0d1cb1419432c82911f7e8094fea9bf3
zero <boy2000007man@gmail.com>
2013-06-30 22:49:23 +0800

    just change a little

--- Paper2/src/Point.h
+++ Paper2/src/Point.h
@@ -1,14 +1,10 @@
 #ifndef __SP_POINT_H__
 #define __SP_POINT_H__
-
-namespace RECTPACKING {
-
+namespace CornerBlockList {
 struct Point
 {
 	int x;
 	int y;
 };
-
-}
-
+}   // namespace CornerBlockList
 #endif	//__SP_POINT_H__
